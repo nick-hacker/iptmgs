badb06fd1deb9759722401479e03f47bec83ad5d
Nickamen <toto2ndu@gmail.com>
2017-05-02 21:23:30 +1400

    Create Models and their associated Controllers Models -Address, Location, Detail, -Institution, College, School, Department, Program, Course -Cartegory, Organization, Post -Staff, Student
    Controllers
    -Institution, College, School, Department, Program, Course
    -Cartegory, Organization, Post
    -Staff, Student
    
    
    Signed-off-by: Nickamen <toto2ndu@gmail.com>

---
+++ app/Http/Controllers/CartegoriesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Cartegory;
+use Illuminate\Http\Request;
+
+class CartegoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Cartegory  $cartegory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cartegory $cartegory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Cartegory  $cartegory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cartegory $cartegory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Cartegory  $cartegory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cartegory $cartegory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Cartegory  $cartegory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cartegory $cartegory)
+    {
+        //
+    }
+}
